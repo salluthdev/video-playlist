e5de46225796792e9c08d1e944b669b1f78d2da7
Faisal L <salluthdev@gmail.com>
2022-05-15 17:58:51 +0700

    Penyimpanan list video baru ke localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-let videos = ["rdBkym-eyIc", "De-pksZy6a0", "avkcvzrvPhM", "M4GkNCHYcvk"];
+let localVideo = localStorage.getItem('list')
+let videos = ["rdBkym-eyIc", "De-pksZy6a0", "avkcvzrvPhM", localVideo]; // M4GkNCHYcvk
 let currentOrder = 0;
-let maxVideo = videos.length - 1;
+
+function getMaxVideo() {
+	return videos.length - 1;
+}
 
 function prev() {
-	currentOrder = (currentOrder == 0) ? 0 : currentOrder - 1;
+	currentOrder = (currentOrder == 0) ? getMaxVideo() : currentOrder - 1;
 	changeVideo();
 }
 
 function next() {
-	currentOrder = (currentOrder == maxVideo) ? maxVideo : currentOrder + 1;
+	currentOrder = (currentOrder == getMaxVideo()) ? 0 : currentOrder + 1;
 	changeVideo();
 }
 
@@ -16,3 +20,22 @@ function changeVideo() {
 	document.querySelector('iframe')
 		.setAttribute('src', 'https://www.youtube.com/embed/' + videos[currentOrder]);
 }
+
+function random() {
+	currentOrder = Math.floor(Math.random() * videos.length);
+	changeVideo();
+}
+
+function addNew() {
+	let inputValue = document.getElementById('inputValue').value;
+	videos.push(inputValue);
+	localStorage.setItem('list', inputValue);
+	
+}
+
+if(localStorage.getItem('list') == null)
+{
+	videos.pop();
+}
+
+console.log(videos)
